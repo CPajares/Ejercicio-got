e2871015ff0c294f2e2d4c477f5acc6ffd22c8a2
Carlos Pajares <c.pajares@hotmail.com>
2021-10-11 12:57:04 +0200

    created class Asesor

---
+++ Asesor.js
@@ -0,0 +1,10 @@
+import Personaje from "./Personaje";
+
+class Rey extends Personaje {
+  asesora_a;
+  frase = "No sé por qué, pero creo que voy a morir pronto";
+  constructor(nombre, familia, edad, asesora_a) {
+    super(nombre, familia, edad);
+    this.asesora_a = asesora_a;
+  }
+}
